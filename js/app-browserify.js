303fc3fe71f16f5170338c66b13e10d90d6c9ed2
Aristone <theariston@gmail.com>
2015-06-04 15:42:20 -0500

    noncommital

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -1,4 +1,4 @@
-"use strict";
+
 // just Node?
 // var fetch = require('node-fetch')
 // Browserify?
@@ -17,49 +17,68 @@ require('whatwg-fetch') //--> not a typo, don't store as a var
 // }
 
 "use strict";
+
 require("es5-shim")
+
 require("babel/register")
 
 var Promise = require('es6-promise').Promise
+
 var $ = require('jquery')
 
-
-
-function qs(selector) {
-    return document.querySelector(selector)
-}
-			
 var key = 'c91eeae561eca7bb95874e5914fbe4d0'
 
 var GPS = new Promise((res, rej) => {
-    // if gps successful, resolve with coordinates
-    // else reject with error
+    
     navigator.geolocation.getCurrentPosition(
-        (gpsData) => res({ lat: gpsData.coords.latitude, lon: gpsData.coords.longitude }),
+        
+        (gpsData) => res({ 
+            lat: gpsData.coords.latitude, 
+            lon: gpsData.coords.longitude }),
         (error) => rej(error.message)
     )
 })
+	
 
 GPS.then((ll) => {
-    var url = `https: api.forecast.io/forecast/${key}/${ll.lat},${ll.lon}?callback=?`
-    var x = $.getJSON(url).then((r) => <li>${key}: ${ll.lat[key]}</li>(r))
-
-
-    qs('.lat ul').innerHtml = x
-    // qs('.long ul').innerHtml = 
-
-// function b(){
-
-//             var apiKey = 'c91eeae561eca7bb95874e5914fbe4d0';
-//             var url = 'https://api.forecast.io/forecast/';
-//             var lati = 0;
-//             var longi = 0;
-//             var data;
-
-//             $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?", function(data) {
-//               //console.log(data);
-//               $('.top').html('and the temperature is: ' + data.currently.temperature);
-//             });
-//         }
     
+    var url = `https: api.forecast.io/forecast/${key}/${ll.lat},${ll.lon}?callback=?`
+    var x = $.getJSON(url).then((result) => ${key}: ${ll.lat[key]}(result))
+
+
+   
+    // $.getJSON("demo_ajax_json.js", function(result){
+    //     $.each(result, function(i, field){
+    //         $("div").append(field + " ");
+
+
+    
+
+
+    var .lat = `${ll.lat}`
+    var .long = `${ll.lon}`
+    var current_temp = current_data.apparentTemperature
+    var current_weather = 
+        
+
+        
+    qs('.lat').innerHtml = 
+    qs('.long').innerHtml =  
+    qs('.temp').innerHtml =
+    qs('.weather').innerHtml =
+
+
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
